f77f63c32f742632c007eaad0d095a0a363b4363
Isabelle Pacheco <isapachecosenai@gmail.com>
2024-11-11 11:26:18 -0300

    store.php

--- store.php
+++ store.php
@@ -5,7 +5,7 @@ include 'conexao.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") { // Verifica se o formulário foi enviado
     $nome = $_POST['nome']; // Recebe o nome
     $endereco = $_POST['endereço']; // Recebe o endereço
-    $data_nascimento = $_POST['nascimento']; // Recebe a data de nascimento
+    $data_nascimento = $_POST['data_nascimento']; // Recebe a data de nascimento
     $telefone = $_POST['telefone']; // Recebe o telefone
     $sql = "INSERT INTO infromações (nome, endereço, data_nascimento, telefone) VALUES ('$nome', '$endereco', '$data_nascimento', '$telefone')";
 
