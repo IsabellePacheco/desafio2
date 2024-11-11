8b996230439f79a8727516bf34b8ac2bce9d5fa4
Isabelle Pacheco <isapachecosenai@gmail.com>
2024-11-11 11:27:31 -0300

    update.php

--- update.php
+++ update.php
@@ -1,22 +1,23 @@
+<link rel="stylesheet" href="update.css">
 <?php
 include 'conexao.php'; // Inclui o arquivo de conexão
 
 if (isset($_GET['id'])) { // Verifica se o ID foi passado
     $id = $_GET['id']; // Recebe o ID
-    $sql = "SELECT * FROM informações.infromações WHERE id=$id"; // Consulta o usuário
+    $sql = "SELECT * FROM infromações WHERE id=$id"; // Consulta o usuário
     $result = $conn->query($sql); // Executa a consulta
-    $usuario = $result->fetch_assoc(); // Obtém os dados do usuário
+    $infromações = $result->fetch_assoc(); // Obtém os dados do usuário
 }
 
 // Se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST['nome']; // Recebe o novo nome
     $endereço = $_POST['endereço']; // Recebe o endereço
-    $data_nascimento = $_POST['nascimento']; // Recebe a data de nascimento
+    $data_nascimento = $_POST['data_nascimento']; // Recebe a data de nascimento
     $telefone = $_POST['telefone']; // Recebe o telefone
-    $sql = "UPDATE informações.infromações SET nome='$nome', endereço='$endereço', nascimento='$data_nascimento' telefone='$telefone' WHERE id= '$id'" ;  // Prepara a atualização
+    $sql = "UPDATE informações.infromações SET nome='$nome', endereço='$endereço', data_nascimento='$data_nascimento', telefone='$telefone' WHERE id= $id" ;  // Prepara a atualização
 
-    if ($conn-->query($sql)===TRUE) {
+    if ($conn->query($sql) === TRUE) {
         header("Location: index.php"); // Redireciona se a atualização for bem-sucedida
     } else {
         echo "Erro: " . $conn->error; // Mostra erro, se houver
@@ -36,13 +37,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <h1>Atualizar Usuário</h1>
     <form action="" method="POST">
         <label>Nome:</label>
-        <input type="text" name="nome" value="<?php echo $usu['nome']; ?>" required>
+        <input type="text" name="nome" value="<?php echo $infromações['nome']; ?>" required>
         <label>Endereço:</label>
-        <input type="text" name="endereço" value="<?php echo $usuario['endereço']; ?>" required>
+        <input type="text" name="endereço" value="<?php echo $infromações['endereço']; ?>" required>
         <label>Data de nascimento:</label>
-        <input type="date" name="nascimento" value="<?php echo $usuario['nascimento']; ?>" required>
+        <input type="date" name="nascimento" value ="<?php echo $infromações['data_nascimento']; ?> " required>
         <label>Telefone:</label>
-        <input type="tel" name="telefone" value="<?php echo $usuario['telefone']; ?>" required>
+        <input type="tel" name="telefone" value="<?php echo $infromações['telefone']; ?>" required>
         <input type="submit" value="Atualizar">
     </form>
     <a href="index.php">Cancelar</a> <!-- Link para voltar -->
